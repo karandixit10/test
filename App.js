b80178419d3978323c1a5cfe1cfbf206f1961172
Karan Dixit <karandixit10@gmail.com>
2021-11-02 22:34:22 +0530

    Initial commit

--- App.js
+++ App.js
@@ -1,21 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  TouchableOpacity,
+  BackHandler,
+  Button,
+  Image
+} from "react-native";
+import { WebView } from "react-native-webview";
+import { Modal, Appbar, Provider, Portal } from "react-native-paper";
+import * as ImagePicker from 'expo-image-picker';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    // <WebView style={styles.container} source={{ uri: "https://in.tradingview.com/chart/" }} />
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Button title="DEPOSIT"/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  header: {
+    backgroundColor: "#212121",
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    paddingVertical: 100,
+    alignContent: "center",
+    backgroundColor: "#F5FCFF",
+  },
+  chart: {
+    flex: 1,
+  },
+  textTitle: {
+    color: "white",
+    fontSize: 13,
+    fontWeight: "bold",
+    paddingLeft: "5%",
+  },
+  textValue: {
+    color: "white",
+    fontSize: 12,
+    fontWeight: "bold",
+    paddingRight: "5%",
   },
 });
